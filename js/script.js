9a5e86a9e47fb27a3cc875007013834b41cc8c4c
soomin <wnghkdsns@naver.com>
2023-07-06 15:50:42 +0900

    6 수정

--- js/script.js
+++ js/script.js
@@ -1,68 +1,80 @@
-//app-btn toggle
-const appBtn=$('.app-btn');
-const bg=$('.mob-bg')
-const mobContent=$('.mob-content')
-appBtn.on({click:function (){
+// app-btn toggle
+const appBtn=$('.appbarBt');
+const bg=$('.mob-bg');
+const mobContent=$('.mob-gnb .mob-content');
+
+appBtn.on({click: function() {
     $('.bar').addClass('active');
-    mobContent.animate({right:0},700)
-    bg.addClass('add-app-btn');
+    bg.addClass('on');
+    mobContent.addClass('active');
+    }
+});
 
-}})
-bg.on({click:function (){
+bg.on({click: function() {
     $('.bar').removeClass('active');
-    mobContent.removeClass('active')
-    bg.removeClass('add-app-btn');
-    mobContent.animate({right:'-70%'},700)
-}})
+    mobContent.removeClass('active');
+    bg.removeClass('on');
+    }
+});
 
-//mob-nav toggle
+
+//mob-nav depth toggle
 const menu = $('.mob-depth1');
 menu.click(function(){
     $(this).next('.mob-depth2').toggleClass('on')
 })
 
 
-//메인 슬라이드 스와이퍼
+//메인 슬라이드
 var swiper = new Swiper(".mySwiper", {
-spaceBetween: 30,
-centeredSlides: true,
-loop:true,
-autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-}, 
-pagination: {
-    el: ".swiper-pagination",
-    type: "progressbar",
-},
-navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-}
-});
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    });
 
 
 
-//main_menu_hot 슬라이드
-$('.owl-carousel').owlCarousel({
-    items: 2,
-    loop: true,
-    margin: 10,
-    nav: true,
-    navText: ["", ""], 
-    dots: true, 
-    autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: true,
+//best menu 슬라이드
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:30,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsiveClass:true,
     responsive:{
         0:{
-            items:1
+            items:1,
+            nav:true
         },
-        600:{
-            items:1
+        680:{
+            items:2,
+            nav:false
         },
-        1000:{
-            items:2
+        768:{
+            items:3,
+            nav:false
+        },
+        1100:{
+            items:4,
+            nav:true,
+            loop:true
         }
-    }
-}) 
+    }    
+});
+
+    
