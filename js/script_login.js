87b70411c9d4b212a841247a196c37de30352362
hsoomin <wnghkdsns@naver.com>
2024-05-26 17:13:03 +0900

    login 수정

--- js/script_login.js
+++ js/script_login.js
@@ -17,7 +17,6 @@ let $pwArea2=document.getElementById('pwArea2');
 let $pwMes2=document.getElementById('pwMes2');
 
 
-
 //이름
 let regName=/^[가-힣a-zA-Z]+$/; 
 $name.addEventListener('blur',function(){
@@ -29,7 +28,6 @@ $name.addEventListener('blur',function(){
         $name.value=''
     }
 }) 
-
 
 //핸드폰번호
 let regPhone=/(^01[01]{1})([0-9]{4})[0-9]{4}$/;
@@ -45,7 +43,6 @@ $phone.addEventListener('blur',function(){
 
 //아이디
 let regid=/^[a-z]+[a-z0-9]{5,19}$/g; //영문자로 시작하는 영문자 또는 숫자 6~20자 
-
 $idArea.addEventListener('blur',function(){  
     if(!regid.test($idArea.value)){ 
         $idMes.innerHTML="아이디를 다시 입력해주세요."
@@ -55,7 +52,6 @@ $idArea.addEventListener('blur',function(){
         $idMes.style.color='green'
     }
 })
-
 
 //이메일
 let regemail=/^[a-zA-Z0-9]([-_.]?\w+)*@[a-zA-Z0-9]([-_.]?[a-zA-Z0-9])*\.[a-zA-Z]{2,3}$/; //[a-zA-Z0-9]으로 시작하고 ([-_.]?\w+)*
@@ -69,9 +65,6 @@ $email.addEventListener('blur',function(){
     }
 })
 
-
-
-
 //비밀번호
 let regPw=/^.*(?=^.{8,12}$)(?=.*\d)(?=.*[!”#$%’()*+,-./:;<=>?@^_|~]).*$/;    
 $pwArea.addEventListener('blur',function(){    
@@ -83,7 +76,6 @@ $pwArea.addEventListener('blur',function(){
         $pwMes.style.color='green'
     }
 })
-
 
 $pwArea2.addEventListener('blur',function(){    
     if(($pwArea.value==$pwArea2.value)&&(!$pwArea2.value=='')){  
@@ -99,7 +91,6 @@ $pwArea2.addEventListener('blur',function(){
         }
     }
 })
-
 
 document.getElementById('submit').onclick=function(){
     if($name.value==''){
@@ -126,7 +117,7 @@ document.getElementById('submit').onclick=function(){
                             alert('비밀번호가 일치하지 않습니다.'); 
                             return false;
                         }else{
-                            location.href='index.html'
+                            location.href = '../sub/join.html';
                         }
                     }
                 }
@@ -134,5 +125,3 @@ document.getElementById('submit').onclick=function(){
         }
     }
 }
-
-
